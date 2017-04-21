94fd7741ae42c836a54df35eb654b7fb768ac94f
Jan Hudec <bulb@ucw.cz>
2017-04-22 00:27:53 +0200

    (cargo-release) generate docs

---
+++ implementors/core/slice/trait.SliceExt.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["libc"] = [];
+implementors["locale_config"] = [];
+implementors["regex_syntax"] = [];
+implementors["void"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
